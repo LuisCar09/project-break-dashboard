8c981e15b2b8f770003febef03efb95c873f5b6f
Luis Carlos <luiscbravo94@gmail.com>
2024-07-16 15:26:44 +0100

    CSS added to links section, Finished minimum requirements

--- src/js/links.js
+++ src/js/links.js
@@ -2,7 +2,8 @@
 const inputSubmit = document.getElementById('inputSubmit')
 
 const itemContainer = document.getElementById('link-container--list')
-
+const backgroundPictures = ['https://images.unsplash.com/photo-1485740112426-0c2549fa8c86?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D','https://images.unsplash.com/photo-1619995745882-f4128ac82ad6?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D','https://images.unsplash.com/photo-1446776709462-d6b525c57bd3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D','https://images.unsplash.com/photo-1457364983758-510f8afa9f5f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D','https://images.unsplash.com/photo-1516850228053-a807778c4e0f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D','https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D','https://images.unsplash.com/photo-1597933437986-5b61315e70fc?q=80&w=2082&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D','https://images.unsplash.com/photo-1519681393784-d120267933ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D','https://img.freepik.com/foto-gratis/fondo-pantalla-abstracto-nebulosa-ultra-detallado-7_1562-752.jpg?t=st=1721137350~exp=1721140950~hmac=f8fffec7cec8c52261ebe43d9b03984a7af1de510cfbfe4d5ff5370024d590d0&w=1380','https://images.unsplash.com/photo-1505852679233-d9fd70aff56d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1533680635069-35927f241ead?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D']
+console.log(backgroundPictures);
 const pageName = document.getElementById('pageName')
 const pageUrl = document.getElementById('url')
 inputSubmit.addEventListener('click',(e) => saveData(e) )
@@ -52,7 +53,14 @@ const deleteItem = (e) => {
     showItems()
 
 }
-
+const changeBackground = () => {
+    const body = document.body
+    const randomNumber = Math.floor(Math.random() * backgroundPictures.length)
+    console.log(randomNumber);
+    body.style.backgroundImage = `url(${backgroundPictures[randomNumber]})`
+    
+}
+setInterval(changeBackground,5000)
 showItems()
 
 
